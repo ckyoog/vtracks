c0bfd55d01939fe6704169df70ee77fbb43dda67
Yu Kou <ykou@fortinet.com>
2015-08-19 17:16:48 -0700

    Fix bug that filesum.sh tries to handle any file types
    Acually, only regular files can be handled (caculated checksum), special
    files can not be caculated checksum. Now filesum.sh just tries to handle
    regular files.

--- src/sample-chroot/about/.verify/.scripts/filesum.sh
+++ src/sample-chroot/about/.verify/.scripts/filesum.sh
@@ -7,7 +7,7 @@ for n in $SKIP; do
 done
 ignore_opt="${o:+-not ( ( $o ) -prune )}"
 
-find -L "$@" $ignore_opt -not -type d -print0 |
+find -L "$@" $ignore_opt -type f -print0 |
 xargs -r -0 $XSUM	# using `xargs' to do something to a huge set of
 			# files can apparently save very much more time
 			# than doing it to each file
