4d592142d6b4835690b92cc8d52261b4e922a431
Yu Kou <ykou@fortinet.com>
2015-08-19 17:07:33 -0700

    Add options '-r' for xargs
    This avoids 'xargs' runs command regardless there is argument or not.
    With this option, xargs will not run command if there is no non-blank
    argument.

--- src/sample-chroot/about/.verify/.scripts/filesum.sh
+++ src/sample-chroot/about/.verify/.scripts/filesum.sh
@@ -14,6 +14,6 @@ for i; do
 		builtin echo -ne $i'\x00'
 	fi
 done |
-xargs -0 $XSUM		# using `xargs' to do something to a huge set of
+xargs -r -0 $XSUM	# using `xargs' to do something to a huge set of
 			# files can apparently save very much more time
 			# than doing it to each file
