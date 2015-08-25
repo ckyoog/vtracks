228fc25285cec9d217cf36e944ddb90677c792eb
Yu Kou <ykou@fortinet.com>
2015-08-25 15:32:00 -0700

    Fix bug that absolute path should be used

--- src/sample-chroot/about/.delta/validate.sh
+++ src/sample-chroot/about/.delta/validate.sh
@@ -5,7 +5,7 @@ DIRBASE=${1:-$DEFBASE}
 NEWDIR=$DIRBASE.new
 OLDDIR=$DIRBASE
 
-FILESUM_SCRIPT=${FILESUM_SCRIPT:-../.verify/.scripts/filesum.sh}
+FILESUM_SCRIPT=$(readlink -f ${FILESUM_SCRIPT:-../.verify/.scripts/filesum.sh})
 
 set -o pipefail
 diff -rq --no-dereference $NEWDIR $OLDDIR &&
