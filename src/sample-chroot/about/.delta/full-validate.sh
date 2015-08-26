1d2344fc9190c162aa345495fc9bfb0caa4add5a
Yu Kou <ckyoog@gmail.com>
2015-08-25 21:59:57 -0700

    Assure scripts can be executed in any directory
    Assure the both 'used-for-validate' scripts can be executed in any
    directory, not only in directory where they are.
    
    Because they call another script whose path relative to them is fixed.
    So to be able to execute them in any directory, I must use command
    'dirname' to extract their directory first, then use the relative path
    to find that script they call.

--- src/sample-chroot/about/.delta/full-validate.sh
+++ src/sample-chroot/about/.delta/full-validate.sh
@@ -5,7 +5,18 @@ DIRBASE=${1:-$DEFBASE}
 NEWDIR=$DIRBASE.new
 OLDDIR=$DIRBASE
 
-FILESUM_SCRIPT=$(readlink -f ${FILESUM_SCRIPT:-../.verify/.scripts/filesum.sh})
+_FILESUM_RELPATH=../.verify/.scripts/filesum.sh
+_FILESUM_DEFPATH=`dirname $0`/$_FILESUM_RELPATH
+# If run this script without any dir prefix, that means it is put in PATH,
+# and that means I can't use 'dirname' directly to get its dir, I have to
+# use command 'which' first to get its absolute path, then use 'dirname',
+# as following,
+#	[ `basename $0` = $0 ] && _FILESUM_DEFPATH=`dirname $(which $0)`/$_FILESUM_RELPATH
+# But actually, if it is put in PATH, the script it calls, filesum.sh,
+# has to be put in PATH too, so I actually don't need to use the relative
+# path to locate filesum.sh any more. It is actually like this,
+#	[ `basename $0` = $0 ] && _FILESUM_DEFPATH=filesum.sh
+FILESUM_SCRIPT=$(readlink -m ${FILESUM_SCRIPT:-$_FILESUM_DEFPATH})
 
 fetch_two_filenames()
 {
