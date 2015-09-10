30a80c433389417cc2d818abbf7eca955b51057c
Yu Kou <ckyoog@gmail.com>
2015-09-09 20:31:32 -0700

    Add /about/.delta/ into need-to-be-version'ed file list

--- src/sample-chroot/about/.verify/.scripts/ce-sum.sh
+++ src/sample-chroot/about/.verify/.scripts/ce-sum.sh
@@ -2,7 +2,7 @@
 
 # !!!Must be called after chroot!!!
 
-export INCL="/bin /sbin /lib* /usr /var/db/pkg /about/.verify/.scripts /chroot.sh /.chroot*.sh"
+export INCL="/bin /sbin /lib* /usr /var/db/pkg /about/.verify/.scripts /about/.delta /chroot.sh /.chroot*.sh"
 #export EXCL="/usr/tmp"	# This will tell filesum-wrapper.sh to remove items in EXCL after hash list is generated
 export SKIP=tmp		# This will tell filesum.sh to skip the names in SKIP
 
