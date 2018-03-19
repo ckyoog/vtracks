23364f9c00e07551081e7ded19e0feaee76018e9
Yu Kou <ckyoog@gmail.com>
2018-03-19 15:40:02 -0700

    Add curl support for forum-posts-search
    curl can use socks5 proxy

--- src/forum-posts-search/x.sh
+++ src/forum-posts-search/x.sh
@@ -1,27 +1,49 @@
 #!/bin/sh
+# See how I use wget and curl with cookies
+
+#alias wget="/Users/hongyan/workbench/wget-1.13.4/src/wget"
 
 if [ "$1" = save-cookies ]; then
-	wget --keep-session-cookies --save-cookies cookies.txt --post-data "formhash=0bbd6af2&referer=http%3A%2F%2F174.127.195.163%2Fbbs%2Findex.php&loginfield=username&username=wantsex1999&password=iwantsex&questionid=0&answer=&cookietime=2592000&loginmode=&styleid=&loginsubmit=true" "http://174.127.195.163/bbs/logging.php?action=login&"
+	if [ -z "$X_USE_PROXY" ]; then
+		wget --keep-session-cookies --save-cookies cookies.txt --post-data "formhash=0bbd6af2&referer=http%3A%2F%2F174.127.195.163%2Fbbs%2Findex.php&loginfield=username&username=wantsex1999&password=iwantsex&questionid=0&answer=&cookietime=2592000&loginmode=&styleid=&loginsubmit=true" "http://174.127.195.163/bbs/logging.php?action=login&"
+	else
+		curl --socks5 127.0.0.1:23456 --cookie-jar cookie.jar --data "formhash=0bbd6af2&referer=http%3A%2F%2F174.127.195.163%2Fbbs%2Findex.php&loginfield=username&username=wantsex1999&password=iwantsex&questionid=0&answer=&cookietime=2592000&loginmode=&styleid=&loginsubmit=true" "http://174.127.195.163/bbs/logging.php?action=login&" > /dev/null
+	fi
 	exit
 elif [ "$1" = test-cookies ]; then
-	wget --load-cookies=cookies.txt "http://174.127.195.163/bbs/forum-230-1.html" -O 1
+	if [ -z "$X_USE_PROXY" ]; then
+		wget --load-cookies=cookies.txt "http://174.127.195.163/bbs/forum-230-1.html" -O 1
+	else
+		curl --socks5 127.0.0.1:23456 --cookie cookie.jar "http://174.127.195.163/bbs/forum-230-1.html" -o 1
+	fi
 	exit
 fi
+
+FORUM_yazhou=230
+FORUM_oumei=229
+FORUM_yazhou_wuma=143
+FORUM=$FORUM_yazhou
 
 BEGIN=`cat begin.txt`
 
 for ((i=$BEGIN; i<=1795; ++i)); do
-	#"http://174.127.195.163/bbs/forumdisplay.php?fid=230&page=$i"
-	wget --load-cookies=cookies.txt "http://174.127.195.163/bbs/forum-230-$i.html" -O $i
+	#"http://174.127.195.163/bbs/forumdisplay.php?fid=${FORUM}&page=$i"
+	sleep 0.5
+	if [ -z "$X_USE_PROXY" ]; then
+		wget --load-cookies=cookies.txt "http://174.127.195.163/bbs/forum-${FORUM}-$i.html" -O $i
+	else
+		curl --socks5 127.0.0.1:23456 --cookie cookie.jar "http://174.127.195.163/bbs/forum-${FORUM}-$i.html" -o $i
+	fi
 
 	if [ -s exclude.txt ]; then
 		while read exclude; do
-			sed -i '\|'$exclude'|d' $i
+			sed -e '\|'$exclude'|d' -i '' $i
 		done < exclude.txt
 	fi
 
 	found=0
-	while read key; do
+	while read -r key; do
+		echo grep $key $i
 		if grep $key $i; then
 			found=1
 			break
